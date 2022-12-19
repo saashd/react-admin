1896a2b30f59e9e9505296b3d33ed65d5085b717
AlexDruzina <alexdruzina@gmail.com>
2022-12-19 10:59:00 +0200

    Added Registration Page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, {ChangeEvent, SyntheticEvent, useState} from "react";
+import '../Login.css'
 
 function Register() {
+    const [state, setState] = useState({firstName: "", lastName: "", email: "", password: "", passwordConfirm: ""})
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setState({...state, [e.target.id]: e.target.value})
 
-    return (<div>Register</div>);
+    };
+    const submit = (e: SyntheticEvent) => {
+        e.preventDefault();
+        console.log(state)
+
+    };
+
+    return (<div>
+        <main className="form-signin w-100 m-auto">
+            <form onSubmit={submit}>
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                <input id="firstName" type="text" className="form-control" placeholder="First Name" required
+                       onChange={handleChange}/>
+                <input id="lastName" type="text" className="form-control" placeholder="Last Name" required
+                       onChange={handleChange}/>
+                <input id="email" type="email" className="form-control" placeholder="name@example.com" required
+                       onChange={handleChange}/>
+                <input id="password" type="password" className="form-control" placeholder="Password" required
+                       onChange={handleChange}/>
+                <input id="passwordConfirm" type="password" className="form-control" placeholder="Password Confirtm"
+                       required
+                       onChange={handleChange}/>
+
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+
+            </form>
+        </main>
+    </div>);
 
 }
 
