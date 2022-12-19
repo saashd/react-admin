6dcb4d6ba362a73abcdb070625071e99a40f4874
AlexDruzina <alexdruzina@gmail.com>
2022-12-19 10:23:06 +0200

    Create init app with users and dashboard components. Import routes and bootstrap style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Menu from "./components/Menu";
+import Users from "./pages/Users";
+import Dashboard from "./pages/Dashboard";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Nav/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <Menu/>
+                        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                            <Routes>
+                                <Route path={'/'} element={<Dashboard/>}/>
+                                <Route path={'/users'} element={<Users/>}/>
+                            </Routes>
+                        </main>
+                    </div>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
